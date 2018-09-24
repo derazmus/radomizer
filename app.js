0260e91884604e9319abe097f0a57ad0a3426eec
Denise Erazmus <denise.erazmus@gmail.com>
2018-09-24 14:56:51 -0500

    initial layout, created array and radom from that array

---
+++ app.js
@@ -0,0 +1,23 @@
+//take data from imputs, put into array, make radom choice from that array and display in results box. 
+
+
+//empty array of choices
+
+var choices = [];
+
+function answer() {
+    event.preventDefault();
+    //choices
+    var choice1 = document.getElementById("choice1").value;
+    var choice2 = document.getElementById("choice2").value;
+    var choice3 = document.getElementById("choice3").value;
+
+    //push to array
+    choices.push(choice1, choice2, choice3);
+
+    //make random choice
+    var random = choices[Math.floor(Math.random() * choices.length)];
+
+    console.log(random)
+
+};
