a0ace9a913c9a6d1f5ef86e0061c29ee2fa79be3
Denise Erazmus <denise.erazmus@gmail.com>
2018-09-29 16:54:28 -0500

    restructure, grabbing item from list input

--- app.js
+++ app.js
@@ -1,47 +1,37 @@
-
-
-//create input fields
-
-// function addInputFields(){
+//take items entered and display on screen in list
+function addToList() {
+    event.preventDefault();
+    var choice = document.getElementById('choiceItems').value;
+    console.log(choice);
+}
+// function changeIt() {
 //     event.preventDefault();
 
-//     //grab number from text field
-//     var numberDecisions = document.getElementById("number").value;
-//     console.log(numberDecisions);
-
-//     //create that number of input fields in the proper div
-//     var div = document.createElement('div')
-
+//     var i = 1;
+//     choiceDiv.innerHTML = choiceDiv.innerHTML + "<input type = 'text' name = 'choice'>"
 // }
 
-function changeIt() {
-    event.preventDefault();
+// //empty array of choices
 
-    var i = 1;
-    choiceDiv.innerHTML = choiceDiv.innerHTML + "<input type = 'text' name = 'choice'>"
-}
+// var choices = [];
 
-//empty array of choices
+// function answer() {
+//     event.preventDefault();
+//     //choices
+//     var choice1 = document.getElementById("choice1").value;
+//     var choice2 = document.getElementById("choice2").value;
+//     var choice3 = document.getElementById("choice3").value;
 
-var choices = [];
+//     //push to array
+//     choices.push(choice1, choice2, choice3);
 
-function answer() {
-    event.preventDefault();
-    //choices
-    var choice1 = document.getElementById("choice1").value;
-    var choice2 = document.getElementById("choice2").value;
-    var choice3 = document.getElementById("choice3").value;
+//     //make random choice
+//     var random = choices[Math.floor(Math.random() * choices.length)];
 
-    //push to array
-    choices.push(choice1, choice2, choice3);
+//     console.log(random);
 
-    //make random choice
-    var random = choices[Math.floor(Math.random() * choices.length)];
+//     //display on HTML
+//     document.getElementById("Decisions").innerHTML = random;
 
-    console.log(random);
 
-    //display on HTML
-    document.getElementById("Decisions").innerHTML = random;
-  	
-
-};
+// };
